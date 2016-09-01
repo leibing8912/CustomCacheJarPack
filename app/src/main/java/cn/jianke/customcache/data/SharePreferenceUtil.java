a8005be9ecbffe0660387f17c49ebf318946da7e
xiaozutian <xiaozutian@192.168.83.34>
2016-09-01 14:59:05 +0800

    Update_modify to a jar pack

--- app/src/main/java/cn/jianke/customcache/data/SharePreferenceUtil.java
+++ app/src/main/java/cn/jianke/customcache/data/SharePreferenceUtil.java
@@ -1,66 +1,31 @@
 package cn.jianke.customcache.data;
-
 import android.content.Context;
 import android.content.SharedPreferences;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import cn.jianke.customcache.utils.StringUtil;
 
-/**
- * @className: SharePreferenceUtil
- * @classDescription:  用户数据读写工具
- * @author: leibing
- * @createTime: 2016/08/26
- */
 public class SharePreferenceUtil {
-    // 数据信息文件名
     private static String DATA_INFO_SHAREDPREFERENCE = "data";
     // sington
     private static SharePreferenceUtil builder;
-    //SharedPreferences Editor对象
+    //SharedPreferences Editor object
     private static SharedPreferences.Editor editor;
-    //SharedPreferences对象
+    //SharedPreferences object
     private static SharedPreferences mPre;
-    // 请求返回缓存list
     private static String SP_LOCAL_CACHE = "sp_local_cache";
 
-    /**
-     * 构造函数
-     * @author leibing
-     * @createTime 2016/08/26
-     * @lastModify 2016/08/26
-     * @param
-     * @return
-     */
     private SharePreferenceUtil(){
     }
 
-    /**
-     * sington 获取数据工具对象
-     * @author leibing
-     * @createTime 2016/08/26
-     * @lastModify 2016/08/26
-     * @param context 上下文
-     * @return
-     */
-    public static SharePreferenceUtil getInstance(Context context) {
+    public synchronized static SharePreferenceUtil getInstance(Context context) {
         return getInstance(context, DATA_INFO_SHAREDPREFERENCE);
     }
 
-    /**
-     * sington 获取数据工具对象
-     * @author leibing
-     * @createTime 2016/08/26
-     * @lastModify 2016/08/26
-     * @param context 上下文
-     * @param fileName 保存到指定的文件的名称
-     * @return
-     */
-    public static SharePreferenceUtil getInstance(Context context, String fileName) {
+    public synchronized static SharePreferenceUtil getInstance(Context context, String fileName) {
         if (builder == null) {
             builder = new SharePreferenceUtil();
         }
-        // 如果传入的文件名称为空，这使用默认文件名
         if (StringUtil.isEmpty(fileName)) {
             fileName = DATA_INFO_SHAREDPREFERENCE;
         }
@@ -70,15 +35,7 @@ public class SharePreferenceUtil {
         return builder;
     }
 
-    /**
-     * 清除文件名下所有的数据
-     * @author leibing
-     * @createTime 2016/08/26
-     * @lastModify 2016/08/26
-     * @param mContext 上下文
-     * @return
-     */
-    public void clearData(Context mContext) {
+    public synchronized void clearData(Context mContext) {
         try {
             SpLocalCache.clear(mContext, getSpLocalCache());
             if (editor != null)
@@ -88,15 +45,7 @@ public class SharePreferenceUtil {
         }
     }
 
-    /**
-     * 存储一个SpLocalCache(名称)，以便于用户缓存的统一管理
-     * @author leibing
-     * @createTime 2016/08/26
-     * @lastModify 2016/08/26
-     * @param cacheName 缓存名
-     * @return
-     */
-    public void setSpLocalCache(String cacheName){
+    public synchronized void setSpLocalCache(String cacheName){
         try {
             Set<String> cacheSet = getSpLocalCache();
             cacheSet.add(cacheName);
@@ -107,14 +56,7 @@ public class SharePreferenceUtil {
         }
     }
 
-    /**
-     * 获取所有的SpLocalCache(名称)
-     * @author leibing
-     * @createTime 2016/08/26
-     * @lastModify 2016/08/26
-     * @return
-     */
-    public Set<String> getSpLocalCache(){
+    public synchronized Set<String> getSpLocalCache(){
         Set<String> result;
         result = mPre.getStringSet(SP_LOCAL_CACHE, new LinkedHashSet<String>());
         return result;
--- app/src/main/java/cn/jianke/customcache/data/SpLocalCache.java
+++ app/src/main/java/cn/jianke/customcache/data/SpLocalCache.java
@@ -1,5 +1,4 @@
 package cn.jianke.customcache.data;
-
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Handler;
@@ -12,148 +11,88 @@ import java.util.Set;
 import cn.jianke.customcache.module.ThreadManager;
 import cn.jianke.customcache.utils.StringUtil;
 
-/**
- * @className: SpLocalCache
- * @classDescription: 存于xml文件中的本地缓存工具, 该缓存直接存储一个请求返回对象，
- * 该缓存归于UserSahrePreferenceUtil管理，当清除用户缓存方法执行时，此缓存下的数据应全部清除。
- * @author: leibing
- * @createTime: 2016/8/26
- */
 public class SpLocalCache<T> {
-    // 缓存Key
     private static final String KEY_DATA = "data";
-    // 缓存名
+
     private String cacheName;
-    // 自定义Handler,用于将子线程数据更新到UI线程
+
     private Handler mHandler = new Handler();
 
-    /**
-     * 清除Set中的缓存
-     * @author leibing
-     * @createTime 2016/8/26
-     * @lastModify 2016/8/26
-     * @param context
-     * @param cacheSet 要删除的缓存
-     * @return
-     */
     public static void clear(final Context context, final Set<String> cacheSet){
         ThreadManager.getInstance().getNewCachedThreadPool().execute(new Runnable() {
             @Override
             public void run() {
-                if (cacheSet == null)
-                    return;
-                for(String cacheName:cacheSet){
-                    SharedPreferences spLc = context.getSharedPreferences(
-                            cacheName,
-                            Context.MODE_PRIVATE
-                    );
-                    spLc.edit().clear().commit();
+                synchronized (SpLocalCache.class) {
+                    if (cacheSet == null)
+                        return;
+                    for (String cacheName : cacheSet) {
+                        SharedPreferences spLc = context.getSharedPreferences(
+                                cacheName,
+                                Context.MODE_PRIVATE
+                        );
+                        spLc.edit().clear().commit();
+                    }
                 }
             }
         });
     }
 
-    /**
-     * 构造函数(用于普通对象缓存)
-     * @author leibing
-     * @createTime 2016/08/26
-     * @lastModify 2016/08/26
-     * @param forWhich 类名 缓存对象类名
-     * @return
-     */
     public SpLocalCache(Class<T> forWhich){
         cacheName = forWhich.getName();
     }
 
-    /**
-     * 构造函数(用于列表缓存)
-     * @author leibing
-     * @createTime 2016/08/26
-     * @lastModify 2016/08/26
-     * @param forWhich     缓存列表类名
-     * @param modelClass  列表中数据类名
-     * @return
-     */
     public SpLocalCache(Class<T> forWhich, Class modelClass){
         cacheName = forWhich.getName() + "_" + modelClass.getName();
-        System.out.println("ddddddddddddddddddd cacheName = " + cacheName);
     }
 
-    /**
-     * Save the data to the local cache.
-     * @author leibing
-     * @createTime 2016/08/26
-     * @lastModify 2016/08/26
-     * @param context 上下文
-     * @param data 数据源
-     * @return
-     */
     public void save(final Context context, final T data){
-                final SharedPreferences spLc = context.getSharedPreferences(
-                        cacheName,
-                        Context.MODE_PRIVATE
-                );
                 ThreadManager.getInstance().getNewCachedThreadPool().execute(new Runnable() {
                     @Override
                     public void run() {
-                        String strData = base64Encode(data);
-                        if (strData != null)
-                            spLc.edit()
-                                    .putString(KEY_DATA, base64Encode(data))
-                                    .commit();
-                        SharePreferenceUtil.getInstance(context).setSpLocalCache(cacheName);
+                        synchronized (SpLocalCache.class) {
+                            final SharedPreferences spLc = context.getSharedPreferences(
+                                    cacheName,
+                                    Context.MODE_PRIVATE
+                            );
+                            String strData = base64Encode(data);
+                            if (strData != null)
+                                spLc.edit()
+                                        .putString(KEY_DATA, base64Encode(data))
+                                        .commit();
+                            SharePreferenceUtil.getInstance(context).setSpLocalCache(cacheName);
+                        }
                     }
                 });
     }
 
-    /**
-     * Read the data from the local cache
-     * @author leibing
-     * @createTime 2016/08/26
-     * @lastModify 2016/08/26
-     * @param context 上下文
-     * @param localCacheCallBack 回调
-     * @return
-     */
     public void read(final Context context,  final LocalCacheCallBack localCacheCallBack){
-        SharedPreferences spLc = context.getSharedPreferences(
-                cacheName,
-                Context.MODE_PRIVATE
-        );
-        final String strData = spLc.getString(KEY_DATA, null);
         ThreadManager.getInstance().getNewCachedThreadPool().execute(new Runnable() {
             @Override
             public void run() {
-                if (StringUtil.isNotEmpty(strData)) {
-                    final Object obj = base64Decode(strData);
-                    mHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (localCacheCallBack != null)
-                                localCacheCallBack.readCacheComplete(obj);
-                        }
-                    });
-                } else {
-                    mHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (localCacheCallBack != null)
-                                localCacheCallBack.readCacheComplete(null);
-                        }
-                    });
+                synchronized (SpLocalCache.class) {
+                    SharedPreferences spLc = context.getSharedPreferences(
+                            cacheName,
+                            Context.MODE_PRIVATE
+                    );
+                    final String strData = spLc.getString(KEY_DATA, null);
+                    if (StringUtil.isNotEmpty(strData)) {
+                        final Object obj = base64Decode(strData);
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (localCacheCallBack != null)
+                                    localCacheCallBack.readCacheComplete(obj);
+                            }
+                        });
+                    }else {
+                        if (localCacheCallBack != null)
+                            localCacheCallBack.readCacheComplete(null);
+                    }
                 }
             }
         });
     }
 
-    /**
-     * 读取Cache的同步方法
-     * @author leibing
-     * @createTime 2016/4/1
-     * @lastModify 2016/4/1
-     * @param context
-     * @return
-     */
     public Object read(final Context context){
         SharedPreferences spLc = context.getSharedPreferences(
                 cacheName,
@@ -164,35 +103,21 @@ public class SpLocalCache<T> {
         return obj;
     }
 
-    /**
-     * clear the data to the local cache
-     * @author leibing
-     * @createTime 2016/08/26
-     * @lastModify 2016/08/26
-     * @param context 上下文
-     * @return
-     */
     public void clear(final Context context) {
                 ThreadManager.getInstance().getNewCachedThreadPool().execute(new Runnable() {
                     @Override
                     public void run() {
-                        SharedPreferences spLc = context.getSharedPreferences(
-                                cacheName,
-                                Context.MODE_PRIVATE
-                        );
-                        spLc.edit().putString(KEY_DATA, "").commit();
+                        synchronized (SpLocalCache.class) {
+                            SharedPreferences spLc = context.getSharedPreferences(
+                                    cacheName,
+                                    Context.MODE_PRIVATE
+                            );
+                            spLc.edit().putString(KEY_DATA, "").commit();
+                        }
                     }
                 });
     }
 
-    /**
-     * 数据base64Encode
-     * @author leibing
-     * @createTime 2016/08/26
-     * @lastModify 2016/08/26
-     * @param data 数据
-     * @return
-     */
     private String base64Encode(T data){
         String result = null;
         try {
@@ -205,14 +130,6 @@ public class SpLocalCache<T> {
         return result;
     }
 
-    /**
-     * 数据base64Decode
-     * @author leibing
-     * @createTime 2016/08/26
-     * @lastModify 2016/08/26
-     * @param strData 数据
-     * @return
-     */
     private static Object base64Decode(String strData){
         Object result = null;
         try {
@@ -224,14 +141,6 @@ public class SpLocalCache<T> {
         return result;
     }
 
-    /**
-     * 对象转字节数组
-     * @author leibing
-     * @createTime 2016/08/26
-     * @lastModify 2016/08/26
-     * @param obj 对象
-     * @return
-     */
     private byte[] getBytesFromObject(T obj) throws Exception{
         if (obj == null){
             return null;
@@ -242,14 +151,6 @@ public class SpLocalCache<T> {
         return bo.toByteArray();
     }
 
-    /**
-     * 字符数组转对象
-     * @author leibing
-     * @createTime 2016/08/26
-     * @lastModify 2016/08/26
-     * @param objBytes 对象字节数组
-     * @return
-     */
     private static Object getObjectFromBytes(byte[] objBytes) throws Exception {
         if (objBytes == null || objBytes.length == 0) {
             return null;
@@ -259,14 +160,7 @@ public class SpLocalCache<T> {
         return oi.readObject();
     }
 
-    /**
-     * @interfaceName: LocalCacheCallBack
-     * @interfaceDescription: 本地缓存回调，用于从子线程中拿缓存更新到UI线程
-     * @author: leibing
-     * @createTime: 2016/08/26
-     */
     public interface LocalCacheCallBack{
-        // 读取缓存完成
         void readCacheComplete(Object obj);
     }
 }
